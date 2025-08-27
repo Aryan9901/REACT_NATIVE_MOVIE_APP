46d4bb2dde495d224290744e4fecabcc4a77020d
Aryan <911aaryan@gmail.com>
2025-08-27 22:20:56 +0530

    removes some images and icon from assets and include some assets and files

---
+++ components/SearchBar.tsx
@@ -0,0 +1,19 @@
+import { icons } from "@/constants/icons";
+import { Image, TextInput, View } from "react-native";
+
+const SearchBar = ({ onPress, placeholder }: { onPress: () => void; placeholder: string }) => {
+	return (
+		<View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+			<Image source={icons.search} tintColor="#AB8BFF" className="size-5" resizeMode="contain" />
+			<TextInput
+				onPress={onPress}
+				placeholder={placeholder}
+				value=""
+				onChangeText={() => {}}
+				placeholderTextColor="#A8B5DB"
+				className="flex-1 ml-2 text-white"
+			/>
+		</View>
+	);
+};
+export default SearchBar;
